f1adeb1140b2273c58d978e3360b3ae6eb66b374
janginseok <589second@gmail.com>
2025-05-22 20:33:32 +0900

    fix: add getAddressFromHeaders

--- lib/firebase/getUser.ts
+++ lib/firebase/getUser.ts
@@ -8,3 +8,12 @@ export async function getFirestoreUser(userId: string) {
   const snap = await getDoc(ref);
   return snap.exists() ? snap.data() : null;
 }
+
+export function getAddressFromHeaders(headers: Headers) {
+  const address =
+    headers.get('x-address') ||
+    headers.get('X-ADDRESS') ||
+    headers.get('address') ||
+    headers.get('ADDRESS');
+  return address ? String(address) : '';
+}
